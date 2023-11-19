1d922c120ced31c9cbef4164369fb902f078fef4
arunmadam1988 <67304925+arunmadam1988@users.noreply.github.com>
2023-11-19 17:28:10 +0000

    Adding new method

--- HelloworldCodeSpaces/Program.cs
+++ HelloworldCodeSpaces/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 int a = 10;
 
-app.MapGet("/", () => "Hello World!" + a);
+app.MapGet("/", GetResult);
+
+string GetResult()
+{
+  return "Hello World"; 
+}
 
 app.Run();
